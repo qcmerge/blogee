2a65dbe7446b969efad24afdf1cef2b66698bb2a
Ben Stafford <bendstafford@gmail.com>
2014-04-25 11:20:57 -0400

    View comments on posts page.

--- spec/factories/post_factory.rb
+++ spec/factories/post_factory.rb
@@ -3,9 +3,5 @@ FactoryGirl.define do
     title 'My Post'
     content 'Lorem ipsum dolor sit amet.'
     author 'Randy Savage'
-
-    factory :post_with_comment do
-      comment
-    end
   end
 end
